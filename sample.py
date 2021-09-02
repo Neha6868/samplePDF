1d736756f6785c4563716dc8b76f89f5a3b33880
neha6868 <neha972335@gmail.com>
2021-09-02 15:57:22 +0530

    remove extra line

--- sample.py
+++ sample.py
@@ -1,17 +1,12 @@
 from PyPDF2 import PdfFileReader
 
-# open the PDF file mypdf
+
 pdfFile = open('sample.pdf', 'rb')
-
-# create PDFFileReader object to read the file
 pdfReader = PdfFileReader(pdfFile)
-
-
 numOfPages = pdfReader.getNumPages()
 
 for i in range(0, numOfPages):
 	pageObj = pdfReader.getPage(i)
 	print(pageObj.extractText())
 	
-# close the PDF file object
 pdfFile.close()
